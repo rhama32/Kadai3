12c2698b26ab9bf59f272bfcc5c014c91a9ce7ee
rhama32 <r.hamaoka32915@gmail.com>
2022-11-27 06:50:45 +0000

    Final

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ before_action :correct_user, only: [:edit, :update]
  def index
   @books = Book.all
   @book = Book.new
+  @user = current_user
  end
 
  def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,17 +14,25 @@ class UsersController < ApplicationController
 
  def edit
   @user = User.find(params[:id])
+  if @user == current_user
+     render "edit"
+  else
+     redirect_to user_path(current_user.id)
+  end
  end
 
  def update
   @user = User.find(params[:id])
-  @user.update(user_params)
+  if @user.update(user_params)
   redirect_to user_path(@user.id)
   flash[:notice] = "successfully"
+  else
+   render :edit
+  end
  end
 
  private
  def user_params
- params.require(:user).permit(:name, :introduction)
+ params.require(:user).permit(:name, :introduction, :profile_image)
  end
 end
